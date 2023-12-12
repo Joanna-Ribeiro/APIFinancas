17d37ce585e5fe18a3fa1f2ab5226d59742dc328
Joanna-Ribeiro <joannac581@gmail.com>
2023-12-12 16:15:53 -0300

    Adicionando index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const rotas  = require('./rotas')
+
+const app = express();
+
+app.use(express.json());
+
+app.use(rotas);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Executando na porta 3000");
+});
