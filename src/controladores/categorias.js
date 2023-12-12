0feb92702c7281c99b5dcf4574f8cd8de021f025
Joanna-Ribeiro <joannac581@gmail.com>
2023-12-12 16:07:36 -0300

    Corrigindo status code

--- src/controladores/categorias.js
+++ src/controladores/categorias.js
@@ -3,10 +3,11 @@ const conexaoPool = require('../config/conexao.js')
 async function listarCategorias(req, res) {
     try {
         const categorias = await conexaoPool.query(
-        "SELECT * FROM categorias"
+            "SELECT * FROM categorias"
         );
 
-        return res.json(categorias.rows);
+        return res.status(200).json(categorias.rows);
+
     } catch (error) {
         return res.status(500).json({ mensagem: "Erro interno do servidor" });
     }
