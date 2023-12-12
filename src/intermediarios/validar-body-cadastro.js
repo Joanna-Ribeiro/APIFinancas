f6add23a36bcb0434015d213978970a5d76d160c
Joanna-Ribeiro <joannac581@gmail.com>
2023-12-12 16:30:30 -0300

    Removendo logs de verificação de erro

--- src/intermediarios/validar-body-cadastro.js
+++ src/intermediarios/validar-body-cadastro.js
@@ -1,8 +1,6 @@
 const verificarCadastroCorpo = (req, res, next) => {
     const { nome, email, senha } = req.body
 
-    console.log('PASSOU AQUI!')
-
     if (!nome || !email || !senha) { 
        
         return res.status(403).json({ "mensagem": "Todos os campos são obrigatórios!" });
