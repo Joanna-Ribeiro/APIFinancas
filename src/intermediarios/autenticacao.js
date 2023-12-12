c1c14a48cccc44f78bdc06d5dd146618dc167029
Joanna-Ribeiro <joannac581@gmail.com>
2023-12-12 15:59:18 -0300

    Intermediario de autenticação

---
+++ src/intermediarios/autenticacao.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken')
+const conexaoPool = require('../config/conexao')
+const senhaJwt = require('../senhaJWT')
+
+const validarUsuarioLogado = async (req, res, next) => {
+	const { authorization } = req.headers
+
+	if (!authorization) {
+		return res.status(401).json({ mensagem: 'Usuário não autorizado' })
+	}
+
+	const token = authorization.split(' ')[1]
+
+	try {
+		const { id } = jwt.verify(token, senhaJwt)
+
+		const { rows, rowCount } = await conexaoPool.query(
+			'select * from usuarios where id = $1',
+			[id]
+		)
+
+		if (rowCount < 1) {
+			return res.status(401).json({ mensagem: 'Não autorizado' })
+		}
+
+		req.usuario = rows[0]
+
+		next()
+        
+	} catch (error) {
+		return res.status(401).json({ mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.' })
+	}
+}
+
+module.exports = {
+	validarUsuarioLogado
+}
